489d14abad238fcd4da3673b523a044d13e5f993
Grace Montenegro <gmontenegro@stackbuilders.com>
2023-05-24 15:13:22 -0500

    Fixing find sintax to find package or cabal files for project.digest hash creation

--- src/scripts/digest.sh
+++ src/scripts/digest.sh
@@ -1,7 +1,7 @@
 # TODO: once committing the cabal file catches on, we can just
 # assume it exists and md5sum *.cabal here.
 find . -maxdepth 1 -type f \
-  -name package.yaml -name '*.cabal' \
+  -name package.yaml -o -name '*.cabal' \
   -exec md5sum {} + > project.digest
 
 git ls-files | xargs md5sum > source.digest
