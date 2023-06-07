10f01e218784f094c914f55b2ee3697cfcf2de24
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-06-07 00:30:50 -0500

    Update build.sh script

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,12 +1,6 @@
-set -x
-
-STACK_YAML=$(circleci env subst "$PARAM_STACK_YAML")
-STACK_ARGUMENTS=$(circleci env subst "$PARAM_STACK_ARGUMENTS")
-
-  # shellcheck disable=SC2086
 stack build \
   --stack-yaml "$STACK_YAML" \
   --no-terminal \
   --test \
   --no-run-tests \
-  $STACK_ARGUMENTS
+  "$STACK_ARGUMENTS"
