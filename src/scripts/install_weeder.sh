563aa2e0f268621fd91a06933c914794b9812403
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-06-07 00:36:10 -0500

    Remove env subst

--- src/scripts/install_weeder.sh
+++ src/scripts/install_weeder.sh
@@ -1,9 +1,5 @@
-STACK_YAML=$(circleci env subst "$PARAM_STACK_YAML")
-STACK_ARGUMENTS=$(circleci env subst "$PARAM_STACK_ARGUMENTS")
-
-# shellcheck disable=SC2086
 stack install \
   --stack-yaml "$STACK_YAML" \
   --no-terminal \
   --copy-compiler-tool weeder \
-  $STACK_ARGUMENTS
+  "$STACK_ARGUMENTS"
--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -1,10 +1,5 @@
-STACK_YAML=$(circleci env subst "$PARAM_STACK_YAML")
-STACK_ARGUMENTS=$(circleci env subst "$PARAM_STACK_ARGUMENTS")
-
-
-# shellcheck disable=SC2086
 stack build \
   --stack-yaml "$STACK_YAML" \
   --no-terminal \
   --test \
-  $STACK_ARGUMENTS
+  "$STACK_ARGUMENTS"
